429e067a348e9746c8240c8789a767dca9fef688
zakzinzan123 <zakzinzan123@gmail.com>
2022-10-30 00:04:12 +0800

    Change  setGridSize and createGrid functions

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const sketchDisplay = document.querySelector('.container-display');
 const gridRange = document.querySelector('#grid-range');
+let gridValue = gridRange.value;
 
 function setGridSize() {
-    document.querySelector('#grid-value1').textContent = this.value;
-    document.querySelector('#grid-value2').textContent = this.value;
+    document.querySelectorAll('.grid-value').forEach(val => {
+        val.textContent = gridValue;
+    });
+
+    setTimeout(() => window.location.reload(), 200);
 }
 
-let gridDivAmount = Math.pow(gridRange.value, 2);
+function createGrid() {
+    for (let i = 0; i < Math.pow(gridRange.value, 2); i++) {
+        const div = document.createElement('div');
+        div.className = 'grid-box';
 
-for (let i = 0; i < gridDivAmount; i++) {
-    sketchDisplay.innerHTML += '<div></div>';
+        sketchDisplay.appendChild(div);
+    }
+
+    document.querySelectorAll('.grid-box').forEach(box => {
+        box.style.width = `calc(100% / ${gridRange.value})`;
+        box.style.height = `calc(100% / ${gridRange.value})`;
+    });
 }
 
-const grids = document.querySelectorAll('.container-display > div');
+createGrid();
 
-grids.forEach(grid => {
-    grid.style.width = `calc(100% / ${gridRange.value})`;
-    grid.style.height = `calc(100% / ${gridRange.value})`;
-});
+// let gridDivAmount = Math.pow(gridRange.value, 2);
+
+// for (let i = 0; i < gridDivAmount; i++) {
+//     sketchDisplay.innerHTML += '<div></div>';
+// }
+
+// const grids = document.querySelectorAll('.container-display > div');
+
+// grids.forEach(grid => {
+//     grid.style.width = `calc(100% / ${gridRange.value})`;
+//     grid.style.height = `calc(100% / ${gridRange.value})`;
+// });
 
 sketchDisplay.addEventListener('mousedown', () => {
-    grids.forEach(grid => {
-        grid.addEventListener('mouseover', function () {
+    document.querySelectorAll('.container-display > div').forEach(box => {
+        box.addEventListener('mouseover', function () {
             this.style.background = 'gray';
         });
     });
