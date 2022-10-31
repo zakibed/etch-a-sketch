18a3bdd1e788124ac6548e758e8a84366da849ec
zakzinzan123 <zakzinzan123@gmail.com>
2022-11-01 01:35:49 +0800

    Add color buttons and changeColorMode function for random colors

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 const sketchDisplay = document.querySelector('.container-display');
 const range = document.querySelector('#grid-range');
 const color = document.querySelector('#color-picker');
+const defaultBtn = document.querySelector('#default-btn');
+const randomBtn = document.querySelector('#random-btn');
+
+let colorMode = true;
 
 
-function outputRange() {
+range.value = 16;
+color.value = '#c9c9c9';
+
+createGrid();
+
+function outputRange () {
     document.querySelectorAll('.grid-value').forEach(val => {
         val.textContent = this.value;
     });
 }
 
-function setGridSize() {
+function setGridSize () {
     sketchDisplay.innerHTML = '';
     createGrid();
 }
 
-function createGrid() {
+function createGrid () {
     const gridSize = Math.pow(range.value, 2);
 
     for (let i = 0; i < gridSize; i++) {
@@ -28,21 +37,38 @@ function createGrid() {
                                         repeat(${range.value}, 1fr)`;
 }
 
-range.value = 16;
-color.value = '#aaaaaa';
-createGrid();
+function changeColorMode() {
+    const gridBoxes = document.querySelectorAll('.grid-box');
 
-sketchDisplay.addEventListener('mousedown', () => {
-    document.querySelectorAll('.grid-box').forEach(box => {
-        box.addEventListener('mouseover', function () {
-            this.style.background = color.value;
-            this.style.borderColor = color.value;
+    if (colorMode === true) {
+
+        gridBoxes.forEach(box => {
+            box.addEventListener('mouseover', function () {
+                this.style.background = color.value;
+                this.style.borderColor = color.value;
+            });
         });
-    });
-});
+
+    } else if (colorMode === false) {
+
+        gridBoxes.forEach(box => {
+            box.addEventListener('mouseover', function () {
+                const r = Math.random() * 255,
+                      g = Math.random() * 255,
+                      b = Math.random() * 255;
+
+                this.style.background = `rgb(${r}, ${g}, ${b})`;
+                this.style.borderColor = `rgb(${r}, ${g}, ${b})`;
+            });
+        });
+
+    }
+}
 
 range.addEventListener('input', outputRange);
 range.addEventListener('click', setGridSize);
 
-    
+sketchDisplay.addEventListener('mousedown', changeColorMode);
 
+randomBtn.addEventListener('click', () => colorMode = false);
+defaultBtn.addEventListener('click', () => colorMode = true);
